b26589f8bd8f6d95e903355053581985f526d114
jackkitte <jackkitte735@gmail.com>
2020-08-12 18:12:56 +0900

    インターネット通信

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
+	"net/http"
 	"os"
 )
 
 func main() {
-	file, err := os.Open("multiwriter.txt")
+	conn, err := net.Dial("tcp", "ascii.jp:80")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-	io.Copy(os.Stdout, file)
+	conn.Write([]byte("GET / HTTP/1.0\r\nHost: ascii.jp\r\n\r\n"))
+	res, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	fmt.Println(res.Header)
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
 }
