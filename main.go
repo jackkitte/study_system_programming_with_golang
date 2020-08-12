f6be61207e2a62620f2b4fe4f41a7641846b8e7b
jackkitte <jackkitte735@gmail.com>
2020-08-12 11:46:18 +0900

    io.MultiWriterのサンプル

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"net/http"
+	"io"
+	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("http.ResponseWriter sample"))
-}
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	file, err := os.Create("multiwriter.txt")
+	if err != nil {
+		panic(err)
+	}
+	writer := io.MultiWriter(file, os.Stdout)
+	io.WriteString(writer, "io.MultiWriter example")
 }
