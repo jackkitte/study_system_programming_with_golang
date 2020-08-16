3579fc47714d946b78350db957913ced0d060e03
jackkitte <jackkitte735@gmail.com>
2020-08-16 17:52:46 +0900

    ストリームを操る（io.MultiReader）

--- main.go
+++ main.go
@@ -1,27 +1,16 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
+	"bytes"
 	"io"
-	"strings"
+	"os"
 )
 
-var csvSource = `13101,"100  ","1000003","ﾄｳｷｮｳﾄ","ﾁﾖﾀﾞｸ","ﾋﾄﾂﾊﾞｼ(1ﾁｮｳﾒ)","東京都","千代田区","一ツ橋（１丁目）",1,0,1,0,0,0
-13101,"101  ","1010003","ﾄｳｷｮｳﾄ","ﾁﾖﾀﾞｸ","ﾋﾄﾂﾊﾞｼ(2ﾁｮｳﾒ)","東京都","千代田区","一ツ橋（２丁目）",1,0,1,0,0,0
-13101,"100  ","1000012","ﾄｳｷｮｳﾄ","ﾁﾖﾀﾞｸ","ﾋﾋﾞﾔｺｳｴﾝ","東京都","千代田区","日比谷公園",0,0,0,0,0,0
-13101,"102  ","1020093","ﾄｳｷｮｳﾄ","ﾁﾖﾀﾞｸ","ﾋﾗｶﾜﾁｮｳ","東京都","千代田区","平河町",0,0,1,0,0,0
-13101,"102  ","1020071","ﾄｳｷｮｳﾄ","ﾁﾖﾀﾞｸ","ﾌｼﾞﾐ","東京都","千代田区","富士見",0,0,1,0,0,0
-`
-
 func main() {
-	reader := strings.NewReader(csvSource)
-	csvReaer := csv.NewReader(reader)
-	for {
-		line, err := csvReaer.Read()
-		if err == io.EOF {
-			break
-		}
-		fmt.Println(line[2], line[6:9])
-	}
+	header := bytes.NewBufferString("----- HEADER -----\n")
+	content := bytes.NewBufferString("Example of io.MultiReader\n")
+	footer := bytes.NewBufferString("----- FOOTER -----\n")
+
+	reader := io.MultiReader(header, content, footer)
+	io.Copy(os.Stdout, reader)
 }
