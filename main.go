493fc120ea10a4e3c4257a2c951ea7abcdf6dcf7
jackkitte <jackkitte735@gmail.com>
2020-08-16 17:31:07 +0900

    テキスト解析（改行/単語で区切る）

--- main.go
+++ main.go
@@ -1,89 +1,22 @@
 package main
 
 import (
-	"bytes"
-	"encoding/binary"
+	"bufio"
 	"fmt"
-	"hash/crc32"
-	"io"
-	"os"
+	"strings"
 )
 
-func textChunk(text string) io.Reader {
-	byteData := []byte(text)
-	var buffer bytes.Buffer
-	binary.Write(&buffer, binary.BigEndian, int32(len(byteData)))
-	buffer.WriteString("tEXt")
-	buffer.Write(byteData)
-	crc := crc32.NewIEEE()
-	io.WriteString(crc, "tEXt")
-	binary.Write(&buffer, binary.BigEndian, crc.Sum32())
-
-	return &buffer
-}
-
-func dumpChunk(chunk io.Reader) {
-	var length int32
-	binary.Read(chunk, binary.BigEndian, &length)
-	buffer := make([]byte, 4)
-	chunk.Read(buffer)
-	fmt.Printf("chunk '%v' (%d bytes)\n", string(buffer), length)
-	if bytes.Equal(buffer, []byte("tEXt")) {
-		rawText := make([]byte, length)
-		chunk.Read(rawText)
-		fmt.Println(string(rawText))
-	}
-}
-
-func readChunks(file *os.File) []io.Reader {
-	var chunks []io.Reader
-
-	file.Seek(8, 0)
-	var offset int64 = 8
-
-	for {
-		var length int32
-		err := binary.Read(file, binary.BigEndian, &length)
-		if err == io.EOF {
-			break
-		}
-		chunks = append(chunks, io.NewSectionReader(file, offset, int64(length)+12))
-		offset, _ = file.Seek(int64(length+8), 1)
-	}
-
-	return chunks
-}
+var source = `1行目
+2行目
+3行目`
 
 func main() {
-	file, err := os.Open("Lenna.png")
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-
-	newFile, err := os.Create("Lenna2.png")
-	if err != nil {
-		panic(err)
-	}
-	defer newFile.Close()
-
-	chunks := readChunks(file)
-	io.WriteString(newFile, "\x89PNG\r\n\x1a\n")
-	io.Copy(newFile, chunks[0])
-	io.Copy(newFile, textChunk("ASCII PROGRAMMING++"))
-
-	for _, chunk := range chunks[1:] {
-		io.Copy(newFile, chunk)
-	}
-
-	file2, err := os.Open("Lenna2.png")
-	if err != nil {
-		panic(err)
-	}
-	defer file2.Close()
-
-	chunks2 := readChunks(file2)
-	for _, chunk := range chunks2 {
-		dumpChunk(chunk)
+	reader := bufio.NewReader(strings.NewReader(source))
+	for {
+		line, err := reader.ReadString('\n')
+		fmt.Printf("%#v\n", line)
+		if err != nil {
+			break
+		}
 	}
 }
