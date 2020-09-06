02f655700e3c8e198c92f71f6bc5784d8f1e4b6e
jackkitte <jackkitte735@gmail.com>
2020-09-06 18:26:56 +0900

    UDPサーバー

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
-	"encoding/json"
-	"os"
+	"fmt"
+	"net"
 )
 
 func main() {
-	encoder := json.NewEncoder(os.Stdout)
-	encoder.SetIndent("", "  ")
-	encoder.Encode(
-		map[string]string{
-			"example": "encoding/json",
-			"hello":   "world",
-		})
+	fmt.Println("Server is running at localhost:8888")
+	conn, err := net.ListenPacket("udp", "localhost:8888")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	buffer := make([]byte, 1500)
+	for {
+		length, remoteAddress, err := conn.ReadFrom(buffer)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Received from %v: %v\n", remoteAddress, string(buffer[:length]))
+		_, err = conn.WriteTo([]byte("Hello from Server"), remoteAddress)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
