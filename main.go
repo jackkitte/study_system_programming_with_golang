cfea1e8e8afa05f6372e39c3cf03896a2f006595
jackkitte <jackkitte735@gmail.com>
2020-09-06 18:08:03 +0900

    パイプライニング

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
@@ -11,25 +12,41 @@ import (
 	"time"
 )
 
-func isGZipAcceptable(request *http.Request) bool {
-	return strings.Index(strings.Join(request.Header["Accept-Encoding"], ","), "gzip") != -1
+func writeToConn(sessionResponses chan chan *http.Response, conn net.Conn) {
+	defer conn.Close()
+	for sessionResponse := range sessionResponses {
+		response := <-sessionResponse
+		response.Write(conn)
+		close(sessionResponse)
+	}
 }
 
-var contents = []string{
-	"これは、私わたしが小さいときに、村の茂平もへいというおじいさんからきいたお話です。",
-	"むかしは、私たちの村のちかくの、中山なかやまというところに小さなお城があって、",
-	"中山さまというおとのさまが、おられたそうです。",
-	"その中山から、少しはなれた山の中に、「ごん狐ぎつね」という狐がいました。",
-	"ごんは、一人ひとりぼっちの小狐で、しだの一ぱいしげった森の中に穴をほって住んでいました。",
-	"そして、夜でも昼でも、あたりの村へ出てきて、いたずらばかりしました。",
+func handleRequest(request *http.Request, resultReceiver chan *http.Response) {
+	dump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+	content := "Hello World\n"
+	response := &http.Response{
+		StatusCode:    200,
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		ContentLength: int64(len(content)),
+		Body:          ioutil.NopCloser(strings.NewReader(content)),
+	}
+	resultReceiver <- response
 }
 
 func processSession(conn net.Conn) {
 	fmt.Printf("Accept %v\n", conn.RemoteAddr())
-	defer conn.Close()
+	sessionResponses := make(chan chan *http.Response, 50)
+	defer close(sessionResponses)
+	go writeToConn(sessionResponses, conn)
+	reader := bufio.NewReader(conn)
 	for {
 		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
-		request, err := http.ReadRequest(bufio.NewReader(conn))
+		request, err := http.ReadRequest(reader)
 		if err != nil {
 			neterr, ok := err.(net.Error)
 			if ok && neterr.Timeout() {
@@ -40,22 +57,9 @@ func processSession(conn net.Conn) {
 			}
 			panic(err)
 		}
-		dump, err := httputil.DumpRequest(request, true)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(string(dump))
-		fmt.Fprintf(conn, strings.Join([]string{
-			"HTTP/1.1 200 OK",
-			"Content-Type: text/plain",
-			"Transfer-Encoding: chunked",
-			"", "",
-		}, "\r\n"))
-		for _, content := range contents {
-			bytes := []byte(content)
-			fmt.Fprintf(conn, "%x\r\n%s\r\n", len(bytes), content)
-		}
-		fmt.Fprintf(conn, "0\r\n\r\n")
+		sessionResponse := make(chan *http.Response)
+		sessionResponses <- sessionResponse
+		go handleRequest(request, sessionResponse)
 	}
 }
 
